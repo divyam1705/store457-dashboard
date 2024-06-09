212ff0ff4d641be8f99b914b15cf063c364867f9
Divyam Arora <89800407+divyam1705@users.noreply.github.com>
2024-06-09 15:44:04 +0530

    hdfc

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,9 +1,11 @@
-import Stripe from "stripe";
+// import Stripe from "stripe";
 import { NextResponse } from "next/server";
-import { stripe } from "@/lib/stripe";
+// import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 import { connect } from "http2";
 import { Product } from "@prisma/client";
+import { Phone } from "lucide-react";
+import axios from "axios";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -19,86 +21,116 @@ export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
-  const { orderedProducts,promoCodeId,userEmail } = await req.json();//changed product id with size
+  const { orderedProducts, promoCodeId, userEmail, userPhone, userAddress } =
+    await req.json(); //changed product id with size
   // console.log(orderedProducts);
-  if (!promoCodeId ) {
+  if (!promoCodeId) {
     return NextResponse.json("Promo Code Id Required", { status: 400 });
   }
-  const productIds= orderedProducts.map((prod:{productId:string,sizeValue:string})=>prod.productId);
+  if (!userEmail) {
+    return NextResponse.json("Email Required", { status: 400 });
+  }
+  if (!userPhone) {
+    return NextResponse.json("Phone Number Required", { status: 400 });
+  }
+  if (!userAddress) {
+    return NextResponse.json("Address Required", { status: 400 });
+  }
+  const productIds = orderedProducts.map(
+    (prod: { productId: string; sizeValue: string }) => prod.productId
+  );
   // console.log(productIds);
 
   if (!productIds || productIds.length === 0) {
     return NextResponse.json("Product Ids are required", { status: 400 });
   }
 
-
   const products = await prismadb.product.findMany({
-    where:{
-        id:{
-            in:productIds
-        }
-    }
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
   });
- const promocode=await prismadb.promoCode.findUnique({
-  where:{
-    id:promoCodeId
+  const promocode = await prismadb.promoCode.findUnique({
+    where: {
+      id: promoCodeId,
+    },
+  });
+  if (!promocode) {
+    return NextResponse.json("Valid Promo Code  Required", { status: 400 });
   }
- })
- if(!promocode){
-  return NextResponse.json("Valid Promo Code  Required", { status: 400 });
- }
-  const line_items:Stripe.Checkout.SessionCreateParams.LineItem[]=[];
-  orderedProducts.forEach((product:{productId:string,sizeValue:string})=>{
-    const cur_prod=products.find(prod=>prod.id===product.productId);
-    if(!cur_prod){return;}
-    // console.log(cur_prod.price);
-    line_items.push({
-        quantity:1,
-        price_data:{
-            currency:"INR",
-            product_data:{
-                name:cur_prod.name+" "+product.sizeValue,
-            },
-            unit_amount:Math.floor(cur_prod.price.toNumber()*100 - Number(promocode?.value)*cur_prod.price.toNumber())
-        }
-    })
-  });
-
+  //this should be changed for hdfc
+  // const line_items:Stripe.Checkout.SessionCreateParams.LineItem[]=[];
+  let description = "";
+  let amount = 0;
+  orderedProducts.forEach(
+    (product: { productId: string; sizeValue: string }) => {
+      const cur_prod = products.find((prod) => prod.id === product.productId);
+      if (!cur_prod) {
+        return;
+      }
+      description += cur_prod.name + " " + product.sizeValue + " ";
+      amount += Math.floor(
+        cur_prod.price.toNumber() * 100 -
+          Number(promocode?.value) * cur_prod.price.toNumber()
+      );
+    }
+  );
 
   const order = await prismadb.order.create({
-    data:{
-      email:userEmail,
-      promoCodeId:promoCodeId,
-        storeId:params.storeId,
-        isPaid:false,
-        orderItems:{
-            create: orderedProducts.map((prod:{productId:string,sizeValue:string})=>({
-                product:{
-                    connect:{
-                        id:prod.productId
-                    }
-                },
-                sizeValue:prod.sizeValue
-            }))
-        }
-    }
-  });
-
-  const session= await stripe.checkout.sessions.create({
-    line_items,
-    mode:"payment",
-    billing_address_collection:"required",
-    phone_number_collection:{
-        enabled:true
+    data: {
+      email: userEmail,
+      promoCodeId: promoCodeId,
+      storeId: params.storeId,
+      isPaid: false,
+      phone: userPhone,
+      address: userAddress,
+      orderItems: {
+        create: orderedProducts.map(
+          (prod: { productId: string; sizeValue: string }) => ({
+            product: {
+              connect: {
+                id: prod.productId,
+              },
+            },
+            sizeValue: prod.sizeValue,
+          })
+        ),
+      },
     },
-    success_url:`${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url:`${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata:{
-        orderId:order.id
-    }
   });
 
-  return NextResponse.json({url:session.url},{
-    headers:corsHeaders
-  })
+  // const session = await stripe.checkout.sessions.create({
+  //   line_items,
+  //   mode: "payment",
+  //   billing_address_collection: "required",
+  //   phone_number_collection: {
+  //     enabled: true,
+  //   },
+  //   success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+  //   cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+  //   metadata: {
+  //     orderId: order.id,
+  //   },
+  // });
+  // console.log("total price", amount);
+  const hdfcOrderData = {
+    orderId: order.id,
+    email: userEmail,
+    phone: userPhone,
+    amount: Math.floor(amount/100),
+    description: description,
+  };
+  // console.log(hdfcOrderData);
+  const session = await axios.post(
+    `${process.env.HDFCBACKENDURL}/initiateJuspayPayment`,
+    hdfcOrderData
+  );
+  return NextResponse.json(
+    { url: session.data?.payment_links?.web },
+    {
+      headers: corsHeaders,
+    }
+  );
 }
--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,4 +1,4 @@
-import Stripe from "stripe";
+// import Stripe from "stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
@@ -6,45 +6,48 @@ import { stripe } from "@/lib/stripe";
 import prismadb from "@/lib/prismadb";
 
 export async function POST(req: Request) {
-  const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
-
-  let event: Stripe.Event;
-
-  try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    );
-  } catch (error: any) {
-    return new NextResponse(`Webhook Errors: ${error.message}`, {
-      status: 400,
-    });
+  const body = await req.json();
+  // console.log(body);
+  if(body.event_name!=="ORDER_SUCCEEDED"){return
+    new NextResponse(`Order Not Succeeded`, {
+          status: 400,
+        });
   }
-  const session = event.data.object as Stripe.Checkout.Session;
-  const address = session?.customer_details?.address;
-  const email= session?.customer_details?.email;
+  // const signature = headers().get("Stripe-Signature") as string;
 
-  const addressComponents = [
-    address?.line1,
-    address?.line2,
-    address?.city,
-    address?.state,
-    address?.postal_code,
-    address?.country,
-  ];
-  const addressString = addressComponents.filter((c) => c !== null).join(", ");
-  if (event.type === "checkout.session.completed") {
+  // let event: Stripe.Event;
+
+  // try {
+  //   event = stripe.webhooks.constructEvent(
+  //     body,
+  //     signature,
+  //     process.env.STRIPE_WEBHOOK_SECRET!
+  //   );
+  // } catch (error: any) {
+  //   return new NextResponse(`Webhook Errors: ${error.message}`, {
+  //     status: 400,
+  //   });
+  // }
+  // const session = event.data.object as Stripe.Checkout.Session;
+  // const address = session?.customer_details?.address;
+  // const email= session?.customer_details?.email;
+
+  // const addressComponents = [
+  //   address?.line1,
+  //   address?.line2,
+  //   address?.city,
+  //   address?.state,
+  //   address?.postal_code,
+  //   address?.country,
+  // ];
+  // const addressString = addressComponents.filter((c) => c !== null).join(", ");
+  // if (event.type === "checkout.session.completed") {
     const order = await prismadb.order.update({
         where:{
-            id:session?.metadata?.orderId,
+            id:body.order_id?.metadata?.orderId,
         },
         data:{
             isPaid: true,
-            address: addressString,
-            phone: session?.customer_details?.phone || "",
-            // email:email||""
         },
         include:{
             orderItems:true
@@ -66,7 +69,7 @@ export async function POST(req: Request) {
             sizeId: prod.sizeId
         }
     });
-    console.log(currentStock);
+    // console.log(currentStock);
       await prismadb.stock.updateMany({
         where:{
           productId:prod.id,
@@ -89,6 +92,6 @@ export async function POST(req: Request) {
     //         isArchived: true
     //     }
     // });
-  }
+  // }
   return new NextResponse(null,{status:200})
 }
